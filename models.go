0b4c868f39edf908b8788f04ce30e0fe996a2687
julianedwards <julian.edwards@mongodb.com>
2023-02-06 15:40:48 -0500

    more cleanup

--- models.go
+++ models.go
@@ -1,6 +1,6 @@
 package plank
 
-// Build is the API response model representing metadata of a Logkeeper build.
+// Build is the API model representing metadata of a Logkeeper build.
 type Build struct {
 	ID            string `json:"id"`
 	Builder       string `json:"builder"`
@@ -10,7 +10,7 @@ type Build struct {
 	Tests         []Test `json:"tests"`
 }
 
-// Test is the API response model representing metadata of a Logkeeper test.
+// Test is the API model representing metadata of a Logkeeper test.
 type Test struct {
 	ID            string `json:"id"`
 	Name          string `json:"name"`
